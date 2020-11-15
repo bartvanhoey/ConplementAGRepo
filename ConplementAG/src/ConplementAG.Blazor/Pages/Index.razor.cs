c9d540e3eba50f4f632fd147abe36046c0685dec
bartvanhoey <bartvanhoey@hotmail.com>
2020-11-15 14:25:17 +0100

    translation

--- ConplementAG/src/ConplementAG.Blazor/Pages/Index.razor.cs
+++ ConplementAG/src/ConplementAG.Blazor/Pages/Index.razor.cs
@@ -3,13 +3,13 @@
     public partial class Index
     {
         public string AzureUrl { get; set; } = "https://portal.azure.com/#@bartvanhoeyhotmail.onmicrosoft.com/resource/subscriptions/59ffed3c-16ac-42bd-be89-71f3fc914213/resourceGroups/rgConplementAG/overview";
-        public  bool GoalTodayIsHidden { get; set; } = false;
-        public  bool TechnologiesIsHidden { get; set; } = true;
+        public  bool IsHiddenGoalToday { get; set; } = false;
+        public  bool IsHiddenTechnologies { get; set; } = true;
 
-        protected void CloseGoalToday()
+        protected void ToggleVisibility()
         {
-            GoalTodayIsHidden = true;
-            TechnologiesIsHidden = false;
+            IsHiddenGoalToday = !IsHiddenGoalToday;
+            IsHiddenTechnologies = !IsHiddenTechnologies;
         }
     }
 }
