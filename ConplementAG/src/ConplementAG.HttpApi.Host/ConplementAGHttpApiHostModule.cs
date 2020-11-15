9fb7483f37fb221392dbc0597fbfdd4cff04c5b7
bartvanhoey <bartvanhoey@hotmail.com>
2020-11-15 14:06:52 +0100

    deutsch

--- ConplementAG/src/ConplementAG.HttpApi.Host/ConplementAGHttpApiHostModule.cs
+++ ConplementAG/src/ConplementAG.HttpApi.Host/ConplementAGHttpApiHostModule.cs
@@ -26,178 +26,179 @@ using Volo.Abp.VirtualFileSystem;
 
 namespace ConplementAG
 {
-    [DependsOn(
-        typeof(ConplementAGHttpApiModule),
-        typeof(AbpAutofacModule),
-        typeof(AbpAspNetCoreMultiTenancyModule),
-        typeof(ConplementAGApplicationModule),
-        typeof(ConplementAGEntityFrameworkCoreDbMigrationsModule),
-        typeof(AbpAspNetCoreMvcUiBasicThemeModule),
-        typeof(AbpAspNetCoreAuthenticationJwtBearerModule),
-        typeof(AbpAccountWebIdentityServerModule),
-        typeof(AbpAspNetCoreSerilogModule)
-    )]
-    public class ConplementAGHttpApiHostModule : AbpModule
+  [DependsOn(
+      typeof(ConplementAGHttpApiModule),
+      typeof(AbpAutofacModule),
+      typeof(AbpAspNetCoreMultiTenancyModule),
+      typeof(ConplementAGApplicationModule),
+      typeof(ConplementAGEntityFrameworkCoreDbMigrationsModule),
+      typeof(AbpAspNetCoreMvcUiBasicThemeModule),
+      typeof(AbpAspNetCoreAuthenticationJwtBearerModule),
+      typeof(AbpAccountWebIdentityServerModule),
+      typeof(AbpAspNetCoreSerilogModule)
+  )]
+  public class ConplementAGHttpApiHostModule : AbpModule
+  {
+    private const string DefaultCorsPolicyName = "Default";
+
+    public override void ConfigureServices(ServiceConfigurationContext context)
     {
-        private const string DefaultCorsPolicyName = "Default";
+      var configuration = context.Services.GetConfiguration();
+      var hostingEnvironment = context.Services.GetHostingEnvironment();
 
-        public override void ConfigureServices(ServiceConfigurationContext context)
-        {
-            var configuration = context.Services.GetConfiguration();
-            var hostingEnvironment = context.Services.GetHostingEnvironment();
-
-            ConfigureUrls(configuration);
-            ConfigureConventionalControllers();
-            ConfigureAuthentication(context, configuration);
-            ConfigureLocalization();
-            ConfigureVirtualFileSystem(context);
-            ConfigureCors(context, configuration);
-            ConfigureSwaggerServices(context);
-        }
-
-        private void ConfigureUrls(IConfiguration configuration)
-        {
-            Configure<AppUrlOptions>(options =>
-            {
-                options.Applications["MVC"].RootUrl = configuration["App:SelfUrl"];
-            });
-        }
-
-        private void ConfigureVirtualFileSystem(ServiceConfigurationContext context)
-        {
-            var hostingEnvironment = context.Services.GetHostingEnvironment();
-
-            if (hostingEnvironment.IsDevelopment())
-            {
-                Configure<AbpVirtualFileSystemOptions>(options =>
-                {
-                    options.FileSets.ReplaceEmbeddedByPhysical<ConplementAGDomainSharedModule>(
-                        Path.Combine(hostingEnvironment.ContentRootPath,
-                            $"..{Path.DirectorySeparatorChar}ConplementAG.Domain.Shared"));
-                    options.FileSets.ReplaceEmbeddedByPhysical<ConplementAGDomainModule>(
-                        Path.Combine(hostingEnvironment.ContentRootPath,
-                            $"..{Path.DirectorySeparatorChar}ConplementAG.Domain"));
-                    options.FileSets.ReplaceEmbeddedByPhysical<ConplementAGApplicationContractsModule>(
-                        Path.Combine(hostingEnvironment.ContentRootPath,
-                            $"..{Path.DirectorySeparatorChar}ConplementAG.Application.Contracts"));
-                    options.FileSets.ReplaceEmbeddedByPhysical<ConplementAGApplicationModule>(
-                        Path.Combine(hostingEnvironment.ContentRootPath,
-                            $"..{Path.DirectorySeparatorChar}ConplementAG.Application"));
-                });
-            }
-        }
-
-        private void ConfigureConventionalControllers()
-        {
-            Configure<AbpAspNetCoreMvcOptions>(options =>
-            {
-                options.ConventionalControllers.Create(typeof(ConplementAGApplicationModule).Assembly);
-            });
-        }
-
-        private void ConfigureAuthentication(ServiceConfigurationContext context, IConfiguration configuration)
-        {
-            context.Services.AddAuthentication()
-                .AddJwtBearer(options =>
-                {
-                    options.Authority = configuration["AuthServer:Authority"];
-                    options.RequireHttpsMetadata = Convert.ToBoolean(configuration["AuthServer:RequireHttpsMetadata"]);
-                    options.Audience = "ConplementAG";
-                    options.BackchannelHttpHandler = new HttpClientHandler
-                    {
-                        ServerCertificateCustomValidationCallback =
-                            HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-                    };
-                });
-        }
-
-        private static void ConfigureSwaggerServices(ServiceConfigurationContext context)
-        {
-            context.Services.AddSwaggerGen(
-                options =>
-                {
-                    options.SwaggerDoc("v1", new OpenApiInfo {Title = "ConplementAG API", Version = "v1"});
-                    options.DocInclusionPredicate((docName, description) => true);
-                });
-        }
-
-        private void ConfigureLocalization()
-        {
-            Configure<AbpLocalizationOptions>(options =>
-            {
-                options.Languages.Add(new LanguageInfo("ar", "ar", "العربية"));
-                options.Languages.Add(new LanguageInfo("cs", "cs", "Čeština"));
-                options.Languages.Add(new LanguageInfo("en", "en", "English"));
-                options.Languages.Add(new LanguageInfo("fr", "fr", "Français"));
-                options.Languages.Add(new LanguageInfo("hu", "hu", "Magyar"));
-                options.Languages.Add(new LanguageInfo("pt-BR", "pt-BR", "Português"));
-                options.Languages.Add(new LanguageInfo("ru", "ru", "Русский"));
-                options.Languages.Add(new LanguageInfo("tr", "tr", "Türkçe"));
-                options.Languages.Add(new LanguageInfo("zh-Hans", "zh-Hans", "简体中文"));
-                options.Languages.Add(new LanguageInfo("zh-Hant", "zh-Hant", "繁體中文"));
-            });
-        }
-
-        private void ConfigureCors(ServiceConfigurationContext context, IConfiguration configuration)
-        {
-            context.Services.AddCors(options =>
-            {
-                options.AddPolicy(DefaultCorsPolicyName, builder =>
-                {
-                    builder
-                        .WithOrigins(
-                            configuration["App:CorsOrigins"]
-                                .Split(",", StringSplitOptions.RemoveEmptyEntries)
-                                .Select(o => o.RemovePostFix("/"))
-                                .ToArray()
-                        )
-                        .WithAbpExposedHeaders()
-                        .SetIsOriginAllowedToAllowWildcardSubdomains()
-                        .AllowAnyHeader()
-                        .AllowAnyMethod()
-                        .AllowCredentials();
-                });
-            });
-        }
-
-        public override void OnApplicationInitialization(ApplicationInitializationContext context)
-        {
-            var app = context.GetApplicationBuilder();
-            var env = context.GetEnvironment();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseAbpRequestLocalization();
-
-            if (!env.IsDevelopment())
-            {
-                app.UseErrorPage();
-            }
-
-            app.UseCorrelationId();
-            app.UseVirtualFiles();
-            app.UseRouting();
-            app.UseCors(DefaultCorsPolicyName);
-            app.UseAuthentication();
-            app.UseJwtTokenMiddleware();
-
-            if (MultiTenancyConsts.IsEnabled)
-            {
-                app.UseMultiTenancy();
-            }
-
-            app.UseIdentityServer();
-            app.UseAuthorization();
-
-            app.UseSwagger();
-            app.UseSwaggerUI(options => { options.SwaggerEndpoint("/swagger/v1/swagger.json", "ConplementAG API"); });
-
-            app.UseAuditing();
-            app.UseAbpSerilogEnrichers();
-            app.UseConfiguredEndpoints();
-        }
+      ConfigureUrls(configuration);
+      ConfigureConventionalControllers();
+      ConfigureAuthentication(context, configuration);
+      ConfigureLocalization();
+      ConfigureVirtualFileSystem(context);
+      ConfigureCors(context, configuration);
+      ConfigureSwaggerServices(context);
     }
+
+    private void ConfigureUrls(IConfiguration configuration)
+    {
+      Configure<AppUrlOptions>(options =>
+      {
+        options.Applications["MVC"].RootUrl = configuration["App:SelfUrl"];
+      });
+    }
+
+    private void ConfigureVirtualFileSystem(ServiceConfigurationContext context)
+    {
+      var hostingEnvironment = context.Services.GetHostingEnvironment();
+
+      if (hostingEnvironment.IsDevelopment())
+      {
+        Configure<AbpVirtualFileSystemOptions>(options =>
+        {
+          options.FileSets.ReplaceEmbeddedByPhysical<ConplementAGDomainSharedModule>(
+                      Path.Combine(hostingEnvironment.ContentRootPath,
+                          $"..{Path.DirectorySeparatorChar}ConplementAG.Domain.Shared"));
+          options.FileSets.ReplaceEmbeddedByPhysical<ConplementAGDomainModule>(
+                      Path.Combine(hostingEnvironment.ContentRootPath,
+                          $"..{Path.DirectorySeparatorChar}ConplementAG.Domain"));
+          options.FileSets.ReplaceEmbeddedByPhysical<ConplementAGApplicationContractsModule>(
+                      Path.Combine(hostingEnvironment.ContentRootPath,
+                          $"..{Path.DirectorySeparatorChar}ConplementAG.Application.Contracts"));
+          options.FileSets.ReplaceEmbeddedByPhysical<ConplementAGApplicationModule>(
+                      Path.Combine(hostingEnvironment.ContentRootPath,
+                          $"..{Path.DirectorySeparatorChar}ConplementAG.Application"));
+        });
+      }
+    }
+
+    private void ConfigureConventionalControllers()
+    {
+      Configure<AbpAspNetCoreMvcOptions>(options =>
+      {
+        options.ConventionalControllers.Create(typeof(ConplementAGApplicationModule).Assembly);
+      });
+    }
+
+    private void ConfigureAuthentication(ServiceConfigurationContext context, IConfiguration configuration)
+    {
+      context.Services.AddAuthentication()
+          .AddJwtBearer(options =>
+          {
+            options.Authority = configuration["AuthServer:Authority"];
+            options.RequireHttpsMetadata = Convert.ToBoolean(configuration["AuthServer:RequireHttpsMetadata"]);
+            options.Audience = "ConplementAG";
+            options.BackchannelHttpHandler = new HttpClientHandler
+            {
+              ServerCertificateCustomValidationCallback =
+                          HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+            };
+          });
+    }
+
+    private static void ConfigureSwaggerServices(ServiceConfigurationContext context)
+    {
+      context.Services.AddSwaggerGen(
+          options =>
+          {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "ConplementAG API", Version = "v1" });
+            options.DocInclusionPredicate((docName, description) => true);
+          });
+    }
+
+    private void ConfigureLocalization()
+    {
+      Configure<AbpLocalizationOptions>(options =>
+      {
+        // options.Languages.Add(new LanguageInfo("ar", "ar", "العربية"));
+        // options.Languages.Add(new LanguageInfo("cs", "cs", "Čeština"));
+        options.Languages.Add(new LanguageInfo("en", "en", "English"));
+        options.Languages.Add(new LanguageInfo("de", "de", "Deutsch"));
+        // options.Languages.Add(new LanguageInfo("fr", "fr", "Français"));
+        // options.Languages.Add(new LanguageInfo("hu", "hu", "Magyar"));
+        // options.Languages.Add(new LanguageInfo("pt-BR", "pt-BR", "Português"));
+        // options.Languages.Add(new LanguageInfo("ru", "ru", "Русский"));
+        // options.Languages.Add(new LanguageInfo("tr", "tr", "Türkçe"));
+        // options.Languages.Add(new LanguageInfo("zh-Hans", "zh-Hans", "简体中文"));
+        // options.Languages.Add(new LanguageInfo("zh-Hant", "zh-Hant", "繁體中文"));
+      });
+    }
+
+    private void ConfigureCors(ServiceConfigurationContext context, IConfiguration configuration)
+    {
+      context.Services.AddCors(options =>
+      {
+        options.AddPolicy(DefaultCorsPolicyName, builder =>
+              {
+                builder
+                          .WithOrigins(
+                              configuration["App:CorsOrigins"]
+                                  .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                                  .Select(o => o.RemovePostFix("/"))
+                                  .ToArray()
+                          )
+                          .WithAbpExposedHeaders()
+                          .SetIsOriginAllowedToAllowWildcardSubdomains()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials();
+              });
+      });
+    }
+
+    public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    {
+      var app = context.GetApplicationBuilder();
+      var env = context.GetEnvironment();
+
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseAbpRequestLocalization();
+
+      if (!env.IsDevelopment())
+      {
+        app.UseErrorPage();
+      }
+
+      app.UseCorrelationId();
+      app.UseVirtualFiles();
+      app.UseRouting();
+      app.UseCors(DefaultCorsPolicyName);
+      app.UseAuthentication();
+      app.UseJwtTokenMiddleware();
+
+      if (MultiTenancyConsts.IsEnabled)
+      {
+        app.UseMultiTenancy();
+      }
+
+      app.UseIdentityServer();
+      app.UseAuthorization();
+
+      app.UseSwagger();
+      app.UseSwaggerUI(options => { options.SwaggerEndpoint("/swagger/v1/swagger.json", "ConplementAG API"); });
+
+      app.UseAuditing();
+      app.UseAbpSerilogEnrichers();
+      app.UseConfiguredEndpoints();
+    }
+  }
 }
