817dbe032897663aeac67be92edfffc8c4a0b350
bartvanhoey <bartvanhoey@hotmail.com>
2020-11-18 09:10:04 +0100

    StarDemotFromHere

--- ConplementAG/src/ConplementAG.Domain/Contacts/Contact.cs
+++ ConplementAG/src/ConplementAG.Domain/Contacts/Contact.cs
@@ -19,6 +19,7 @@ namespace ConplementAG.Domain.Contacts
 
    // TODO 1. Run AbpHelper tool to generate CRUD infrastructure
 
-   // TODO 2. Open a terminal in the DbMigrations Project and run command dotnet ef migrations add Contact_Added
+   // TODO 2. Open a terminal in the DbMigrations Project and run command below (Select command + hit F1)
+   // dotnet ef migrations add Contact_Added
 
 }
